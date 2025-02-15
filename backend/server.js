9e9f97f4a41d2484a35950054683146ad9e79345
Praveenkumar K <praveenkrishh01@gmail.com>
2025-02-15 21:43:38 +0530

    server,env,vercer updated

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ config();
 const app = express();
 
 const APP_URL = process.env.APP_URL || 'http://localhost:3000';
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({ origin: APP_URL, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' }));
@@ -19,7 +18,7 @@ connectDB();
 app.use('/images', express.static('images'));
 app.use("/api/auth", router);
 
-app.listen(PORT, () => {
+app.listen(5000, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`App URL: ${APP_URL}`);
 });
