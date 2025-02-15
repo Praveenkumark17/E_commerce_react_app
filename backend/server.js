a26f32a09dc17ddaf9ddb7a4a97b250c1639458e
Praveenkumar K <praveenkrishh01@gmail.com>
2025-02-15 21:14:01 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,10 @@ import router from "./router/authRouter.js";
 config();
 const app = express();
 
+const APP_URL = process.env.APP_URL || 'http://localhost:3000';
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:APP_URL,methods:'GET,HEAD,PUT,PATCH,POST,DELETE'}));
 connectDB();
 
 app.use('/images',express.static('images'))
@@ -18,5 +20,6 @@ app.use("/api/auth", router);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in port ${process.env.PORT}`);
+  console.log(`App running in port ${process.env.APP_URL}`);
 });
 
