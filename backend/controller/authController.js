8263f0faeeaffffa914832fd82e9a761f510c852
Praveenkumar K <praveenkrishh01@gmail.com>
2025-02-16 17:15:47 +0530

    header added

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -44,6 +44,8 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const origin = req.headers.origin;
+    res.header('Access-Control-Allow-Origin', origin);
   try {
     const { email, password } = req.body;
     const finduser = await user.findOne({ email });
