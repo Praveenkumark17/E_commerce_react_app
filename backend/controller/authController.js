3b9ff4cfa21fced19650bdb31f111b0f6715d951
Praveenkumar K <praveenkrishh01@gmail.com>
2025-02-18 21:10:33 +0530

    import update

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 import multer from "multer";
 import fs from 'fs';
+import { put } from "@vercel/blob";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
